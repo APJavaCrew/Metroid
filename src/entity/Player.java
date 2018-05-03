692ead4cda9646a0c35200e79e8fd01098871d32
Nolan <nolan0117@comcast.net>
2018-05-03 09:48:03 -0400

    Weird Changes I don't remember
    Still works though so not bad changes

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -10,7 +10,7 @@ import sprite.SpriteSheet;
 public class Player extends Being {
 
 	SpriteSheet sandman = new SpriteSheet("SamWalkLeft.png", 32, 37);
-	Animation walkLeft = new Animation(sandman.getSpritesAt("10-0-1-2-3-4-5-6-7-8-9-10", "0-0-0-0-0-0-0-0-0-0"), 30, true);
+	Animation walkLeft = new Animation(sandman.getSpritesAt("10-0-1-2-3-4-5-6-7-8-9-10", "0-0-0-0-0-0-0-0-0-0"), 170, true);
 	
 	public Player() {
 		walkLeft.start();
@@ -18,10 +18,8 @@ public class Player extends Being {
 	
 	@Override
 	public void draw(Graphics g) {
-		int newW = walkLeft.getSprite().getWidth() * 5, newH = walkLeft.getSprite().getHeight() * 5;
+		int newW = walkLeft.getSprite().getWidth() * 4, newH = walkLeft.getSprite().getHeight() * 4;
 		Image temp = walkLeft.getSprite().getScaledInstance(newW, newH, Image.SCALE_REPLICATE);
-//		Image wow = currentSprite.getScaledInstance(currentSprite.getWidth() * 5, currentSprite.getHeight() * 5, 0);
-//		currentSprite = new BufferedImage(newW, newH, (Integer) null);
 		g.drawImage(sandman.getSpriteAt(0, 0), 100, 100, null);
 		g.drawImage(temp, 200, 100, null);
 		walkLeft.update();
