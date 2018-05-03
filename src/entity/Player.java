9420675df12b4e7b253576e73def51741e8f1d59
Nolan <nolan0117@comcast.net>
2018-05-03 10:16:57 -0400

    Fixed Animation
    Mostly works now

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -10,7 +10,7 @@ import sprite.SpriteSheet;
 public class Player extends Being {
 
 	SpriteSheet sandman = new SpriteSheet("SamWalkLeft.png", 32, 37);
-	Animation walkLeft = new Animation(sandman.getSpritesAt("10-0-1-2-3-4-5-6-7-8-9-10", "0-0-0-0-0-0-0-0-0-0"), 170, true);
+	Animation walkLeft = new Animation(sandman.getSpritesAt("10-0-1-2-3-4-5-6-7-8-9-10", "0-0-0-0-0-0-0-0-0-0"), 300, true);
 	
 	public Player() {
 		walkLeft.start();
