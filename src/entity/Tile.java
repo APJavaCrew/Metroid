30e196eeaa987b3afe907b42d646ded3eeed92e4
Nolan <nolan0117@comcast.net>
2018-05-04 10:00:34 -0400

    Added Indexes

--- src/entity/Tile.java
+++ src/entity/Tile.java
@@ -5,12 +5,19 @@ import java.awt.Image;
 
 public class Tile extends Entity {
 
+	
 	public Tile() {
 		super();
 	}
 	
 	public Tile(double x, double y) {
 		super(x, y, 20, 20);
+	}
+	
+	public Tile(int x, int y, double xP, double yP) {
+		super(x, y, 20, 20);
+		xIndex = x;
+		yIndex = y;
 	}
 	
 	@Override
