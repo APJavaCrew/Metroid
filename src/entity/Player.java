a89bc5e10a60502589a15a4aee15a4e4d25cb267
ianisdakoolkid <IMHuff566@pccsk12.local>
2018-05-04 10:05:46 -0400

    movement functions in player
    cleaner now

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -3,19 +3,20 @@ package entity;
 import java.awt.Graphics;
 import java.awt.Image;
 
+import main.Constants;
 import sprite.Animation;
 import sprite.SpriteSheet;
 
 public class Player extends Being {
-
+	
 	SpriteSheet sandman = new SpriteSheet("SamWalkLeft.png", 32, 37);
-	Animation walkLeft = new Animation(sandman.getSpritesAt("10-0-1-2-3-4-5-6-7-8-9-10", "0-0-0-0-0-0-0-0-0-0"), 300, true);
-
+	Animation walkLeft = new Animation(sandman.getSpritesAt("10-0-1-2-3-4-5-6-7-8-9-10", "0-0-0-0-0-0-0-0-0-0"), 1, true);
 	
 	private int x, y;
 	private double dx, dy;
 	
 	public Player() {
+		
 		walkLeft.start();
 		
 		x = 0;
@@ -33,11 +34,51 @@ public class Player extends Being {
 	
 	@Override
 	public void draw(Graphics g) {
-		int newW = walkLeft.getSprite().getWidth() * 4, newH = walkLeft.getSprite().getHeight() * 4;
-		Image temp = walkLeft.getSprite().getScaledInstance(newW, newH, Image.SCALE_REPLICATE);
+		g.drawImage(walkLeft.getSprite(), 200, 100, null);
+		g.drawImage(sandman.getSpritesAt("1-0", "0")[0], 200, 100, null);
 		g.drawImage(sandman.getSpriteAt(0, 0), 100, 100, null);
-		g.drawImage(temp, 200, 100, null);
+		g.drawImage(walkLeft.get(), 200, 100, null);
 		walkLeft.update();
+		move();
+	}
+	
+	public void left() {
+		if(Math.abs(dx) > Constants.MAX_RUN_SPEED) {
+			dx -= Constants.RUN_ACCEL;
+		} else {
+			dx = -1;
+		}
+	}
+	
+	public void right() {
+		if(Math.abs(dx) < Constants.MAX_RUN_SPEED) {
+			dx += Constants.RUN_ACCEL;
+		} else {
+			dx = 1;
+		}
+	}
+	
+	public void decelerate() {
+		if(dx > Constants.BRAKE_DEADZONE)
+			dx -= Constants.BRAKE_ACCEL;
+		else if(dx < (-1 * Constants.BRAKE_DEADZONE))
+			dx += Constants.BRAKE_ACCEL;
+		else
+			dx = 0;
+	}
+	
+	public void jump() {
+		dy = -1;
+	}
+	
+	public void fall() {
+		if(dy < Constants.TERMINAL_VELOCITY /*&& player is not on the ground*/) {
+			dy += Constants.GRAVITY_ACCEL;
+		} else if(dy >= Constants.TERMINAL_VELOCITY /*&& player is not on the ground*/) {
+			dy = Constants.TERMINAL_VELOCITY;
+		} else /*player is on the ground*/ {
+			dy = 0;
+		}
 	}
 	
 	@Override
--- src/main/Runner.java
+++ src/main/Runner.java
@@ -53,50 +53,24 @@ public class Runner extends JPanel implements KeyListener {
 	@SuppressWarnings("unused")
 	@Override
 	public void keyPressed(KeyEvent arg0) {
-		if(KeyEvent.VK_A == 1 && KeyEvent.VK_D != 1) {
-			if(Math.abs(player.getDx()) < Constants.MAX_RUN_SPEED) {
-				player.setDx(player.getDx() - Constants.RUN_ACCEL);
-			} else {
-				player.setDx(-1);
-			}
-			
-		} else if(KeyEvent.VK_D == 1 && KeyEvent.VK_A != 1) {
-			if(Math.abs(player.getDx()) < Constants.MAX_RUN_SPEED) {
-				player.setDx(player.getDx() + Constants.RUN_ACCEL);
-			} else {
-				player.setDx(1);
-			}
-		} else if(KeyEvent.VK_D != 1 && KeyEvent.VK_A != 1) {
-			if(player.getDx() > Constants.BRAKE_DEADZONE)
-				player.setDx(player.getDx() - Constants.BRAKE_ACCEL);
-			else if(player.getDx() < (-1 * Constants.BRAKE_DEADZONE))
-				player.setDx(player.getDx() + Constants.BRAKE_ACCEL);
-			else
-				player.setDx(0);
-		}
-		if(KeyEvent.VK_SPACE == 1 /* && [standing on ground] [temp -->]*/ && player.getDy() == 0) {
-			player.setDy(1);
-		}
+		if(KeyEvent.VK_A == 1 && KeyEvent.VK_D != 1)
+			player.left();
+		else if(KeyEvent.VK_D == 1 && KeyEvent.VK_A != 1)
+			player.right();
+			else if(KeyEvent.VK_D != 1 && KeyEvent.VK_A != 1) 
+			player.decelerate();
+		if(KeyEvent.VK_SPACE == 1 /* && [standing on ground] [temp -->]*/ && player.getDy() == 0)
+			player.jump();
 		
 		
 	}
 
 	@Override
 	public void keyReleased(KeyEvent arg0) {
-		if(player.getDx() > Constants.BRAKE_DEADZONE)
-			player.setDx(player.getDx() - Constants.BRAKE_ACCEL);
-		else if(player.getDx() < (-1 * Constants.BRAKE_DEADZONE))
-			player.setDx(player.getDx() + Constants.BRAKE_ACCEL);
-		else
-			player.setDx(0);
-		if(player.getDy() < Constants.TERMINAL_VELOCITY /*&& player is not on the ground*/) {
-			player.setDy(player.getDy() + Constants.GRAVITY_ACCEL);
-		} else if(player.getDy() >= Constants.TERMINAL_VELOCITY /*&& player is not on the ground*/) {
-			player.setDy(Constants.TERMINAL_VELOCITY);
-		} else /*player is on the ground*/ {
-			player.setDy(0);
+		player.decelerate();
+		if(/*player is not on the ground [temp -->]*/true) {
+			player.fall();
 		}
-		
 	}
 
 	@Override
