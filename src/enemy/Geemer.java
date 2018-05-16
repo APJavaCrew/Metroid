9baf5098273eca8d253b4a4e853ff4b3652ae132
TanTan616 <TJAvery616@pccsk12.local>
2018-05-16 10:16:45 -0400

    Geemer Enemy sprite and class update

--- src/enemy/Geemer.java
+++ src/enemy/Geemer.java
@@ -1,8 +1,15 @@
 package enemy;
 
 import entity.Enemy;
+import sprite.SpriteSheet;
 
 public class Geemer extends Enemy {
+	double health=15;
+	
+	double attack=10;
+	SpriteSheet sandman = new SpriteSheet("SamWalkLeft.png", 32, 37);
+	
+	
 	
 
 }
